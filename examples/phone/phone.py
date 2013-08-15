20cb3fa31c8a2692be578368bd4cf80b01d9b46a
Andy Chase <asperous2@gmail.com>
2013-08-14 17:23:42 -0700

    Fix phone example

--- examples/phone/phone.py
+++ examples/phone/phone.py
@@ -4,12 +4,24 @@ sys.path.append('../..')
 from functions import functions
 import reparse
 
-patterns_path = "patterns.yaml"
-expressions_path = "expressions.yaml"
+# If file was imported, include that path
+path = ""
+if '__file__' in globals():
+    import os
+    path = str(os.path.dirname(__file__)) + "/"
+
+patterns_path = path + "patterns.yaml"
+expressions_path = path + "expressions.yaml"
 patterns = reparse.build_from_yaml(functions, expressions_path, patterns_path)
 
 
 def parse(line):
+    """
+    >>> parse('+974-584-5656')
+    [phone(area_code='974', prefix='584', body='5656', fax=False)]
+    >>> parse('Fax: +974-584-5656')
+    [phone(area_code='974', prefix='584', body='5656', fax=True)]
+    """
     output = None
     highest_order = 0
     for pattern in patterns:
@@ -20,5 +32,6 @@ def parse(line):
                 highest_order = pattern.order
     return output
 
-print parse(' +974-584-5656 ')
-print parse(' Fax: +974-584-5656 ')
+if __name__ == "__main__":
+    print parse(' +974-584-5656 ')
+    print parse(' Fax: +974-584-5656 ')
