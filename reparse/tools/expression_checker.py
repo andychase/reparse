9da5d18b1329165f695e85bcaa64dcfc34720c31
Andy Chase <asperous2@gmail.com>
2013-08-27 11:47:45 -0700

    Expression_checker ignores expressions w/o 'Matches' and 'Non-Matches' fields.

--- reparse/tools/expression_checker.py
+++ reparse/tools/expression_checker.py
@@ -1,9 +1,7 @@
 """
 This module contains a useful function that aids in validating
 expression files by making sure each expression in a group
-contains the 'Expression', 'Matches', & 'Non-Matches' fields.
-
-It also runs the fields against the expression to make sure they are valid.
+are valid against the 'Expression', 'Matches', & 'Non-Matches' fields.
 
 It is intended to be used against a ``testing_framework`` that
 contains assertIn, assertTrue, assertFalse (such as unittest).
@@ -18,10 +16,9 @@ Example Usage::
             expression_tester(self, load_yaml("parse/cool/expressions.yaml"))
 """
 from reparse.config import expression_sub
-base_error_msg = "RE|PARSE Failure: In Ex Type {}, Group {}, "
-key_error_msg = base_error_msg + "Could not find Key {}"
-match_error_msg = base_error_msg + "Could not match {}"
-non_match_error_msg = base_error_msg + "Should not match {}"
+base_error_msg = "Expression Type [{}], Group [{}], "
+match_error_msg = base_error_msg + "Could not match [{}]"
+non_match_error_msg = base_error_msg + "Should not match [{}]"
 
 
 def check_expression(testing_framework, expression_dict):
@@ -36,14 +33,13 @@ def check_expression(testing_framework, expression_dict):
     ...   {'class': {'group' :{'Matches': " 0 | 1", 'Non-Matches': "2 | 0 2", 'Expression': "[0-1]"}}})
     """
     for expression_type_name, expression_type in expression_dict.items():
-        for name, expression_object in expression_type .items():
-            for key in ['Matches', 'Non-Matches', 'Expression']:
-                testing_framework.assertIn(key, expression_object.keys(),
-                                           key_error_msg.format(expression_type_name, name, key))
-            for test in expression_object['Matches'].split('|'):
-                # Substitute and check to make sure that the entire string matches
-                result = expression_sub(expression_object['Expression'], '', test.strip()) == ''
-                testing_framework.assertTrue(result, match_error_msg.format(expression_type_name, name, test))
-            for test in expression_object['Non-Matches'].split('|'):
-                result = expression_sub(expression_object['Expression'], '', test.strip()) == ''
-                testing_framework.assertFalse(result, non_match_error_msg.format(expression_type_name, name, test))
+        for name, expression_object in expression_type.items():
+            if 'Matches' in expression_object.keys():
+                for test in expression_object['Matches'].split('|'):
+                    # Substitute and check to make sure that the entire string matches
+                    result = expression_sub(expression_object['Expression'], '', test.strip()) == ''
+                    testing_framework.assertTrue(result, match_error_msg.format(expression_type_name, name, test))
+            if 'Non-Matches' in expression_object.keys():
+                for test in expression_object['Non-Matches'].split('|'):
+                    result = expression_sub(expression_object['Expression'], '', test.strip()) == ''
+                    testing_framework.assertFalse(result, non_match_error_msg.format(expression_type_name, name, test))
