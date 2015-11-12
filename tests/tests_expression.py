2b4605275250e471a296b7eec7f573efbc11de10
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2015-11-12 23:23:49 +0000

    Fixes landscape errors for test_expression file.

--- tests/tests_expression.py
+++ tests/tests_expression.py
@@ -5,6 +5,6 @@ class TestExpression(TestCase):
 
     def test_raises_useful_exception(self):
         """Expression has to raise readable error message."""
-        exp = Expression('inalid (\d]', {}, [], lambda x: x)
+        exp = Expression(r'inalid (\d]', {}, [], lambda x: x)
         with self.assertRaises(exp.InvalidPattern):
-            exp.pattern
+            assert not exp.pattern
