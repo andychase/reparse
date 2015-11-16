da24c5d3d8f0b6aff56af3f293f1b214e5318cfc
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2015-11-16 23:17:05 +0000

    Moves unit tests from reparse source code.

--- tests/tests_expression.py
+++ tests/tests_expression.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from reparse.expression import Expression
+from reparse.expression import AlternatesGroup, Expression
 
 class TestExpression(TestCase):
 
@@ -8,3 +8,40 @@ class TestExpression(TestCase):
         exp = Expression(r'inalid (\d]', {}, [], lambda x: x)
         with self.assertRaises(exp.InvalidPattern):
             assert not exp.pattern
+
+    def test_expressions(self):
+        def greeting(input):
+            def func(greeting):
+                return greeting
+            return func(*input)
+
+        def final(input):
+            for i in input:
+                if i is not None:
+                    return i
+
+        regex = "(hi)|(hi)"
+
+        exp = Expression(regex, [greeting, greeting], [1, 1], final)
+
+        self.assertIsInstance(exp, Expression)
+        self.assertEquals(exp.findall("hi"), ["hi"])
+
+    def test_groups(self):
+        def greeting(input):
+            def func(greeting):
+                return greeting
+            return func(*input)
+
+        def final(input):
+            for i in input:
+                if i is not None:
+                    return i
+        regex = "(hi)"
+        exp = Expression(regex, [greeting], [1], final)
+
+        regex = "(ho)"
+        exp2 = Expression(regex, [greeting], [1], final)
+
+        grouped_expressions = AlternatesGroup([exp, exp2], final)
+        self.assertEquals(grouped_expressions.findall("hi"), ["hi"])
