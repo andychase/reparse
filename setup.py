457bb9f9231243dad25034fc049b1c86443e2616
Andy Chase <asperous2@gmail.com>
2015-11-22 14:45:25 -0800

    Bump setup.py verison 3.0

--- setup.py
+++ setup.py
@@ -8,33 +8,40 @@ def readme_or_docstring():
     if os.path.isfile(path):
         return open(path).read()
     else:
-        import reparse
+        try:
+            import reparse
+            return reparse.__doc__
+        except ImportError:
+            return 'Regular Expression based parsers for extracting data from natural language'
 
-        return reparse.__doc__
 
-setup(name='reparse',
-      version='2.1',
-      description='Sane Regular Expression based parsers',
-      long_description=readme_or_docstring(),
-      author='Andy Chase',
-      author_email='theandychase@gmail.com',
-      url='http://github.com/andychase/reparse',
-      download_url='https://github.com/andychase/reparse/archive/master.zip',
-      license='MIT',
-      packages=['reparse'],
-      install_requires=[
-          'regex',
-          'pyyaml'
-      ],
-      classifiers=(
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Natural Language :: English',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Text Processing'
-      ),
-      )
+setup(
+    name='reparse',
+    version='3.0',
+    description='Regular Expression based parsers for extracting data from natural language',
+    long_description=readme_or_docstring(),
+    author='Andy Chase',
+    author_email='theandychase@gmail.com',
+    url='http://github.com/andychase/reparse',
+    download_url='https://github.com/andychase/reparse/archive/master.zip',
+    license='MIT',
+    packages=['reparse'],
+    install_requires=[
+        'regex',
+        'pyyaml'
+    ],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing'
+    ),
+)
