6ce8190e49970c674e9f6bb8fffc396036d1872a
Andy Chase <asperous2@gmail.com>
2016-11-12 12:10:18 -0800

    Fix examples not being included in doctest

--- examples/colortime/colortime.py
+++ examples/colortime/colortime.py
@@ -1,7 +1,6 @@
-from __future__ import unicode_literals
 """ Example from docs:
 
->>> colortime_parser("~ ~ ~ go to the store ~ buy green at 11pm! ~ ~")  # doctest: +IGNORE_UNICODE
+>>> colortime_parser("~ ~ ~ go to the store ~ buy green at 11pm! ~ ~")
 [('green', datetime.time(23, 0))]
 
 In this case the processing functions weren't specified but you
@@ -9,25 +8,27 @@ still get a useful result as a default.
 >>> colortime_parser("~ ~ ~ Crazy 2pm green ~ ~")  # doctest: +IGNORE_UNICODE
 [['green']]
 """
+from __future__ import unicode_literals
+
 # Example stuff -----------------------------------------------------
 # Have to add the parent directory just in case you
 # run this file in the demo directory without installing Reparse
 import sys
+
 sys.path.append('../..')
 
 # If file was imported, include that path
 path = ""
 if '__file__' in globals():
     import os
+
     path = str(os.path.dirname(__file__))
     if path:
         path += "/"
 
-
 # Reparse ----------------------------------------------------------
 from examples.colortime.functions import functions
 import reparse
-
 
 colortime_parser = reparse.parser(
     parser_type=reparse.basic_parser,
--- examples/phone/phone.py
+++ examples/phone/phone.py
@@ -1,24 +1,26 @@
-from __future__ import unicode_literals
 """ Example of a phone number parser
 >>> phone_parser('+974-584-5656')  # doctest: +IGNORE_UNICODE
 [phone(area_code='974', prefix='584', body='5656', fax=False)]
 >>> phone_parser('Fax: +974-584-5656')  # doctest: +IGNORE_UNICODE
 [phone(area_code='974', prefix='584', body='5656', fax=True)]
 """
+from __future__ import unicode_literals
+
 # Example stuff -----------------------------------------------------
 # Have to add the parent directory just in case you
 # run this file in the demo directory without installing Reparse
 import sys
+
 sys.path.append('../..')
 
 # If file was imported, include that path
 path = ""
 if '__file__' in globals():
     import os
+
     path = str(os.path.dirname(__file__))
     if path:
         path += "/"
-
 
 # Reparse ----------------------------------------------------------
 from examples.phone.functions import functions
@@ -30,7 +32,6 @@ phone_parser = reparse.parser(
     patterns_yaml_path=path + "patterns.yaml",
     functions=functions
 )
-
 
 if __name__ == "__main__":
     print(phone_parser(' +974-584-5656 '))
